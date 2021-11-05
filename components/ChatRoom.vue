404014aeea4aa6392f5506e18cc7394bfc5bed30
Luis Villén <l.villenjaldon@gmail.com>
2021-11-05 11:53:03 +0100

    Adds TailwindCSS and creates first component

---
+++ components/ChatRoom.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mx-auto bg-purple-300">
+    <h1>Here will go my chat!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <ChatRoom/>
 </template>
 
 <script>
